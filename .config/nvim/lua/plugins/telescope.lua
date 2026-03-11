07858265cc797e618ee61f80f6333355f76b7e68
andy <andydevs123@gmail.com>
2026-03-11 10:21:29 -0700

    More improvements on quick file picker in telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,60 +5,32 @@ return {
   branch = 'master',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+    {
       'nvim-telescope/telescope-fzf-native.nvim',
-
-      -- `build` is used to run some command when the plugin is installed/updated.
-      -- This is only run then, not every time Neovim starts up.
       build = 'make',
-
-      -- `cond` is a condition used to determine whether this plugin should be
-      -- installed and loaded.
       cond = function()
         return vim.fn.executable 'make' == 1
       end,
     },
     { 'nvim-telescope/telescope-ui-select.nvim' },
-
-    -- Useful for getting pretty icons, but requires a Nerd Font.
-    -- { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
-  config = function()
-    -- Telescope is a fuzzy finder that comes with a lot of different things that
-    -- it can fuzzy find! It's more than just a "file finder", it can search
-    -- many different aspects of Neovim, your workspace, LSP, and more!
-    --
-    -- The easiest way to use Telescope, is to start by doing something like:
-    --  :Telescope help_tags
-    --
-    -- After running this command, a window will open up and you're able to
-    -- type in the prompt window. You'll see a list of `help_tags` options and
-    -- a corresponding preview of the help.
-    --
-    -- Two important keymaps to use while in Telescope are:
-    --  - Insert mode: <c-/>
-    --  - Normal mode: ?
-    --
-    -- This opens a window that shows you all of the keymaps for the current
-    -- Telescope picker. This is really useful to discover what Telescope can
-    -- do as well as how to actually do it!
 
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
-    require('telescope').setup {
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
-      --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+  config = function()
+    local telescope = require 'telescope'
+    local builtin = require 'telescope.builtin'
+    local actions = require 'telescope.actions'
+    local themes = require 'telescope.themes'
+    local pickers = require 'telescope.pickers'
+    local finders = require 'telescope.finders'
+    local conf = require('telescope.config').values
+
+    telescope.setup {
       defaults = {
         path_display = {
           shorten = 4,
         },
       },
+
       pickers = {
         find_files = {
           hidden = true,
@@ -73,38 +45,191 @@ return {
           },
         },
       },
+
       extensions = {
         ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
+          themes.get_dropdown(),
         },
       },
     }
 
-    -- Enable Telescope extensions if they are installed
-    pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'ui-select')
+    pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'ui-select')
 
-    -- Seeget_dropdown { `:help telescope.builtin`
-    local builtin = require 'telescope.builtin'
-    local actions = require 'telescope.actions'
-    local themes = require 'telescope.themes'
+    local session_recent_files = {}
 
+    local function normalize_path(path)
+      if not path or path == '' then
+        return nil
+      end
+
+      return vim.fs.normalize(vim.fn.fnamemodify(path, ':p'))
+    end
+
+    local function to_project_path(path, cwd)
+      local normalized = normalize_path(path)
+      local normalized_cwd = normalize_path(cwd or vim.loop.cwd())
+      if not normalized or not normalized_cwd then
+        return nil
+      end
+
+      if normalized == normalized_cwd then
+        return '.'
+      end
+
+      local prefix = normalized_cwd .. '/'
+      if normalized:find(prefix, 1, true) == 1 then
+        return normalized:sub(#prefix + 1)
+      end
+
+      return nil
+    end
+
+    local function list_oldfiles()
+      local seen = {}
+      local recent = {}
+
+      for _, file in ipairs(vim.v.oldfiles) do
+        if file and file ~= '' then
+          local normalized = normalize_path(file)
+          if normalized and vim.fn.filereadable(normalized) == 1 and not seen[normalized] then
+            seen[normalized] = true
+            table.insert(recent, normalized)
+          end
+        end
+      end
+
+      return recent
+    end
+
+    local function push_session_recent(path)
+      local normalized = normalize_path(path)
+      if not normalized or vim.fn.filereadable(normalized) ~= 1 then
+        return
+      end
+
+      local updated = { normalized }
+      for _, file in ipairs(session_recent_files) do
+        if file ~= normalized and vim.fn.filereadable(file) == 1 then
+          table.insert(updated, file)
+        end
+      end
+
+      session_recent_files = updated
+    end
+
+    local oldfiles_group = vim.api.nvim_create_augroup('telescope-session-recents', { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
+      group = oldfiles_group,
+      callback = function(args)
+        if vim.bo[args.buf].buftype ~= '' then
+          return
+        end
+
+        push_session_recent(vim.api.nvim_buf_get_name(args.buf))
+      end,
+    })
+
+    ---------------------------------------------------------------------
+    -- SMART FILE PICKER (recent files + project files)
+    ---------------------------------------------------------------------
+
+    local function smart_files(opts)
+      opts = opts or {}
+
+      local cwd = vim.loop.cwd()
+
+      -- get project files
+      local handle = io.popen "rg --files --hidden --glob '!**/.git/*' --glob '!node_modules/**'"
+
+      local project_files = {}
+      if handle then
+        for file in handle:lines() do
+          local project_path = to_project_path(file, cwd)
+          if project_path then
+            table.insert(project_files, project_path)
+          end
+        end
+        handle:close()
+      end
+
+      -- get recent files from current project
+      local recent = {}
+      for _, file in ipairs(session_recent_files) do
+        local project_path = to_project_path(file, cwd)
+        if project_path then
+          table.insert(recent, project_path)
+        end
+      end
+
+      -- merge lists (recent first)
+      local seen = {}
+      local results = {}
+
+      for _, f in ipairs(recent) do
+        if not seen[f] then
+          seen[f] = true
+          table.insert(results, f)
+        end
+      end
+
+      for _, f in ipairs(project_files) do
+        if not seen[f] then
+          seen[f] = true
+          table.insert(results, f)
+        end
+      end
+
+      pickers
+        .new(
+          opts,
+          themes.get_dropdown(vim.tbl_extend('force', {
+            prompt_title = 'Files',
+
+            finder = finders.new_table {
+              results = results,
+            },
+
+            sorter = conf.file_sorter(opts),
+
+            previewer = conf.file_previewer(opts),
+
+            path_display = {
+              shorten = 4,
+            },
+
+            mappings = {
+              n = {
+                ['j'] = actions.move_selection_next,
+                ['k'] = actions.move_selection_previous,
+              },
+            },
+          }, opts))
+        )
+        :find()
+    end
+
+    ---------------------------------------------------------------------
+    -- KEYMAPS
+    ---------------------------------------------------------------------
+
+    -- Navigate recent files first
     vim.keymap.set('n', '<C-p>', function()
-      builtin.oldfiles(themes.get_dropdown {
-        cwd_only = true,
+      smart_files {
         initial_mode = 'normal',
-        mappings = {
-          n = {
-            ['j'] = actions.move_selection_next,
-            ['k'] = actions.move_selection_previous,
-          },
-        },
-      })
-    end, { desc = 'Search Recent Files' })
+      }
+    end, { desc = 'Files (recent first)' })
 
+    -- Search immediately
     vim.keymap.set('n', '<C-m>', function()
-      builtin.oldfiles { cwd_only = true }
-    end, { desc = 'Recent Files (Project)' })
+      smart_files {
+        initial_mode = 'insert',
+      }
+    end, { desc = 'Files (search)' })
+
+    ---------------------------------------------------------------------
+    -- STANDARD TELESCOPE MAPS
+    ---------------------------------------------------------------------
 
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
@@ -114,30 +239,24 @@ return {
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find existing buffers' })
 
-    -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        -- winblend = 10,
+      builtin.current_buffer_fuzzy_find(themes.get_dropdown {
         previewer = false,
       })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    end, { desc = 'Search in current buffer' })
 
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
     vim.keymap.set('n', '<leader>s/', function()
       builtin.live_grep {
         grep_open_files = true,
         prompt_title = 'Live Grep in Open Files',
       }
-    end, { desc = '[S]earch [/] in Open Files' })
+    end, { desc = 'Search in Open Files' })
 
-    -- Shortcut for searching your Neovim configuration files
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [N]eovim files' })
+    end, { desc = 'Search Neovim files' })
   end,
 }
